ff112157f1881c1ab1e2d0c795d5f29ff2a9deec
zhanghuan <chunshui@outlook.com>
2020-02-19 11:22:20 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnGitDemo
+//
+//  Created by zhanghuan on 2020/2/19.
+//  Copyright © 2020 zhanghuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
